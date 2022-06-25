9d7cea9b5f864df6a304b3e382fe6c3d674c09f5
Icaro de Barros <icarodebarros@gmail.com>
2022-06-24 22:31:34 -0300

    Creating end-points for transactions

--- api/src/index.js
+++ api/src/index.js
@@ -30,4 +30,40 @@ app.get('/test-connection', (_req, res) => { // testing db connection
     });
 });
 
+/**
+ * Get all the financil movements
+ */
+app.get('/transactions', (_req, res) => {
+    connection.query('SELECT * FROM cnab', (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.send(results);
+    });
+});
+
+/**
+ * Save a list of financial movements
+ */
+app.post('/transactions', (req, res) => {
+    const list = req.body;
+
+    const sql = 'INSERT INTO cnab (type, datetime, value, cpf, card, ownerName, storeName) VALUES ';
+    const values = list.map((item) => {
+        const dtNorm = item.datetime.replace('T', ' ').substring(0, 19);
+        return `(${item.type}, '${dtNorm}', ${item.value}, ${item.cpf}, '${item.card}', '${item.ownerName}', '${item.storeName}')`
+    });
+    const query = sql + values + ';';
+
+    connection.query(query, (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).send({ 
+            message: 'Records saved successfully',
+            info: results.info
+        });
+    });
+});
+
 app.listen(9001, '0.0.0.0', () => console.log('Listening on port 9001'));
