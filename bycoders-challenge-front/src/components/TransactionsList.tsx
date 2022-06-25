eea6809dbd44eaa6c2138d80e6b41eaf50d56dc4
Icaro de Barros <icarodebarros@gmail.com>
2022-06-24 22:22:31 -0300

    Adding the filtered list

--- bycoders-challenge-front/src/components/TransactionsList.tsx
+++ bycoders-challenge-front/src/components/TransactionsList.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import { FinancialMovement } from '../models/financialMovement';
+import { TransactionTypes } from '../utils/transaction-types';
 import classes from './TransactionsList.module.css';
 
 interface TransactionsListProps {
@@ -33,6 +34,46 @@ const TransactionsList: React.FC<TransactionsListProps> = (props) => {
         setFilteredTransactions(props.transactions.filter(t => t.storeName === store));
     }
 
+    const getTransactionsList = () => {
+        if(!!filteredTransactions.length) {
+          const list = filteredTransactions.map(t => (
+            <div className={classes.transactionItem} key={t.datetime.getTime() + t.value}>
+                <div>Card: <span>{t.card}</span></div>
+                <div>CPF: <span>{t.cpf}</span></div>
+                <div>Date/hour: <span>{t.datetime.toLocaleString("pt-BR")}</span></div>
+                <div>Type: <span>{TransactionTypes.get(t.type)?.description}</span></div>
+                <div>Value: <span className={TransactionTypes.get(t.type)?.inOut === '+' ?
+                  classes.valueG : classes.valueR}>{formatCurrency(t.value)}</span>
+                </div>
+            </div>
+          ));
+          const {storeName, ownerName} = filteredTransactions[0];
+          const total = getTotal();
+    
+          return (
+            <React.Fragment>
+              <div className={classes.transactionHeader}>{storeName} <span>({ownerName})</span></div>
+              {list}
+              <p className={classes.total}>Total: <span className={total >= 0 ?
+                  classes.valueG : classes.valueR}>{formatCurrency(total)}</span></p>
+            </React.Fragment>
+          );
+    
+        } else {
+          return <p className={classes.noStore}>No Sotre selected</p>;
+        }
+    }
+
+    const getTotal = () => {
+        return filteredTransactions.reduce((pre, cur) => {
+          return TransactionTypes.get(cur.type)?.inOut === '+' ? pre + cur.value : pre - cur.value;
+        }, 0);
+    };
+    
+    const formatCurrency = (value: number) => {
+        return value.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL', minimumFractionDigits: 2});
+    };
+
     return (
         <React.Fragment>
             <div className={'card ' + classes.select}>
@@ -40,6 +81,10 @@ const TransactionsList: React.FC<TransactionsListProps> = (props) => {
                     <option value='emptySelect' key='select' disabled>Select a Store...</option>
                     {storeOptions}
                 </select>
+            </div>
+
+            <div className='card'>
+                {getTransactionsList()}
             </div>
         </React.Fragment>
     );
