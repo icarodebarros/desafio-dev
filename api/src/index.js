5d7b962fd83722f6f572051d0bac55bccf6754c5
Icaro de Barros <icarodebarros@gmail.com>
2022-06-24 21:06:12 -0300

    Installing and connecting with mysql

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const app = express();
 
+const connection = mysql.createConnection({
+    host: 'mysql-container',
+    user: 'root',
+    database: 'challengedb',
+    password: 'admin'
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error('Erro ao conectar:', err)
+    }
+});
+
 app.get('/hello', (_req, res) => res.send('Hello World!')); // test
+
+app.get('/test-connection', (_req, res) => { // testing db connection
+    connection.query('SELECT version()', (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.send(results);
+    });
+});
 
 app.listen(9001, '0.0.0.0', () => console.log('Listening on port 9001'));
