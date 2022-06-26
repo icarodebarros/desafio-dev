e76211ee395bd94874b43e6275f868814ce9a207
Icaro de Barros <icarodebarros@gmail.com>
2022-06-26 15:55:51 -0300

    Starting unit tests

--- bycoders-challenge-front/src/App.test.tsx
+++ bycoders-challenge-front/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders ByCodersTec header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/ByCodersTec/i);
   expect(linkElement).toBeInTheDocument();
 });
