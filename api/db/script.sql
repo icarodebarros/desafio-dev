269a80ef98f238f388c41bd2cdb328e4b7eee65a
Icaro de Barros <icarodebarros@gmail.com>
2022-06-26 18:36:51 -0300

    Simplifying startup sql after sequelize

--- api/db/script.sql
+++ api/db/script.sql
@@ -1,14 +1,16 @@
 CREATE DATABASE IF NOT EXISTS challengedb;
 USE challengedb;
 
-CREATE TABLE IF NOT EXISTS cnab (
-    id INT(11) AUTO_INCREMENT,
-    type SMALLINT(2),
-    datetime DATETIME,
-    value DECIMAL(10, 2),
-    cpf BIGINT(11),
-    card VARCHAR(12),
-    ownerName VARCHAR(255),
-    storeName VARCHAR(255),
-    PRIMARY KEY (id)
-);
+-- ** After Sequelize there is no need to create the table via sql script **
+
+-- CREATE TABLE IF NOT EXISTS cnab (
+--     id INT(11) AUTO_INCREMENT,
+--     type SMALLINT(2),
+--     datetime DATETIME,
+--     value DECIMAL(10, 2),
+--     cpf BIGINT(11),
+--     card VARCHAR(12),
+--     ownerName VARCHAR(255),
+--     storeName VARCHAR(255),
+--     PRIMARY KEY (id)
+-- );
