bf59200ecfad1a1e07b31e4f07d8358d0346975c
Icaro de Barros <icarodebarros@gmail.com>
2022-06-26 18:34:32 -0300

    Creating financialMovement controller

---
+++ api/src/controllers/transaction.js
@@ -0,0 +1,29 @@
+const FinancialMovement = require('../models/financialMovement');
+
+exports.getAllTransactions = async (_req, res, _next) => {
+  try {
+    const transactions = await FinancialMovement.findAll();
+    res.status(200).send(transactions);
+  } catch(err) {
+    res.status(500).send('Something went wrong');
+  }
+};
+
+exports.postListTransactions = async (req, res, _next) => {
+  const list = req.body;
+
+  console.log('list', list)
+
+  try {
+    const result = await FinancialMovement.bulkCreate(list);
+    res.status(200).send({ 
+      message: 'Records saved successfully',
+      info: `Rows affected: ${result.length}`
+    });
+  } catch(err) {
+    res.status(500).send({ 
+      message: 'Error saving data',
+      info: err
+    });
+  }
+};
