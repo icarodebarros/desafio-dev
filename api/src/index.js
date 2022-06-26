fde4c70384eff5b8b93ae396fb0aad9ec810c1ab
Icaro de Barros <icarodebarros@gmail.com>
2022-06-26 18:41:06 -0300

    Adjusting index to new file structure

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const mysql = require('mysql2');
 const bodyParser = require('body-parser');
+
+const sequelize = require('./utils/database');
+const FinancialMovement = require('./models/financialMovement');
+
+const testRoutes = require('./routes/test');
+const transactionRoutes = require('./routes/transaction');
 
 const app = express();
 
@@ -12,64 +17,19 @@ app.use((_req, res, next) => { // Avoid CORS policy
     next();
 });
 
-const connection = mysql.createConnection({
-    host: 'mysql-container',
-    user: 'root',
-    database: 'challengedb',
-    password: 'admin'
-});
+app.use('/test', testRoutes);
+app.use('/transactions', transactionRoutes);
 
-connection.connect(err => {
-    if (err) {
-        console.error('Erro ao conectar:', err)
-    }
-});
-
-app.get('/hello', (_req, res) => res.send('Hello World!')); // test
-
-app.get('/test-connection', (_req, res) => { // testing db connection
-    connection.query('SELECT version()', (error, results) => {
-        if (error) {
-            throw error;
-        }
-        res.send(results);
+sequelize
+    .sync()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+        app.listen(
+            9001, 
+            '0.0.0.0', 
+            () => console.log('Listening on port 9001')
+        );
+    })
+    .catch(() => {
+        console.error('Unable to connect to the database:', error);
     });
-});
-
-/**
- * Get all the financil movements
- */
-app.get('/transactions', (_req, res) => {
-    connection.query('SELECT * FROM cnab', (error, results) => {
-        if (error) {
-            throw error;
-        }
-        res.send(results);
-    });
-});
-
-/**
- * Save a list of financial movements
- */
-app.post('/transactions', (req, res) => {
-    const list = req.body;
-
-    const sql = 'INSERT INTO cnab (type, datetime, value, cpf, card, ownerName, storeName) VALUES ';
-    const values = list.map((item) => {
-        const dtNorm = item.datetime.replace('T', ' ').substring(0, 19);
-        return `(${item.type}, '${dtNorm}', ${item.value}, ${item.cpf}, '${item.card}', '${item.ownerName}', '${item.storeName}')`
-    });
-    const query = sql + values + ';';
-
-    connection.query(query, (error, results) => {
-        if (error) {
-            throw error;
-        }
-        res.status(200).send({ 
-            message: 'Records saved successfully',
-            info: results.info
-        });
-    });
-});
-
-app.listen(9001, '0.0.0.0', () => console.log('Listening on port 9001'));
